757ce4d9dd1f265837fc7af5d39f1e045380b3e7
Jan Randa <jnranda@gmail.com>
2024-03-05 20:50:43 +0100

    ADD: NPM publish

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import { PDF } from "./class/PDF.js";
+import { elementDefine } from "./elements/elements.js";
 
 import "./elements/loadDefault.js";
-
 
 export {
     PDF
 }
+
+
 
 export default PDF;
